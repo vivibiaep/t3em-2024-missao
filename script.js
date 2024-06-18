4fe319a4bd51a1093816850a606113506e831f62
vivibiaep <115494889+vivibiaep@users.noreply.github.com>
2024-06-18 11:01:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlterantivas = document.querySelector("caixa-alterativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("caixa-resultado");
+
+const perguntas =
+       {
+         enunciado: "Pergunta 1",
+         alternativas: [
+              "Alternativa 1",
+              "Aternativa 2"
+            ]
+         {      
+         }
+         enunciado: "Pergunta 2",
+         alternativas: [
+              "Alternativa 1",
+              "Aternativa 2"
+            ]
+         {      
+            
+
+
+
+       }
